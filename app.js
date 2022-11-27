bbf60fd212a8028711029df3e5797cfec1491da8
Ilya Yudin <nibezo.cs@gmail.com>
2022-11-27 22:52:25 +0300

    feat: add local storage usage

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+let sessions = document.querySelector('.value').value;
+document.addEventListener('DOMContentLoaded', setSessions());
+
+function setSessions() {
+  document.querySelector('.result').innerHTML =
+    ((parseInt(localStorage.getItem('value')) * 25) / 60).toFixed(1) +
+    ' hour(s)';
+  document.querySelector('.value').value = localStorage.getItem('value');
+}
+
 function calculate() {
-  let sessions = document.querySelector('.value').value;
-  document.querySelector('.result').innerHTML = `${((sessions * 25) / 60).toFixed(1)} hour(s)`;
+  sessions = document.querySelector('.value').value;
+  // Save count of sessions to localStorage
+  localStorage.setItem('value', sessions.toString());
+  document.querySelector('.result').innerHTML = `${(
+    (sessions * 25) /
+    60
+  ).toFixed(1)} hour(s)`;
 }
